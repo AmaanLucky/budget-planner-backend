dda35c549c4a2567cef28b7f82c62fc77b9ae784
AmaanLucky <amaanahmed2405@gmail.com>
2025-07-31 20:09:51 +0530

    allowed origins set

--- server.js
+++ server.js
@@ -12,7 +12,28 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = [
+  'https://wealthio.onrender.com',
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 
 app.use("/auth", authRoutes);
 app.use("/expenses", expenseRoutes);
